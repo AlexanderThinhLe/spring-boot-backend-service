c511b0104991649406b6a85a430118b459d04c40
alexanderthinh <alexanderthinh@gmail.com>
2025-07-01 00:58:24 +0700

    Init Spring Boot base project

---
+++ src/test/java/thinh/springboot/BackendServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package thinh.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
